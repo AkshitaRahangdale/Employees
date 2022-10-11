b379b3c145913a674b7b9400ad4a55badc2bf8b7
AkshitaRahangdale <akshitarahangdale@1720gmail.com>
2022-10-11 12:36:18 +0530

    Add project files.

---
+++ Employees/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Static e1 = new Static("emp1", 23000);
+            Static e2 = new Static("emp2", 234000);
+            Static e3 = new Static("emp3", 235000);
+            Console.WriteLine(e1);
+            Console.WriteLine(e2);
+            Console.WriteLine(e3);
+            Console.WriteLine(Static.TotalCount());
+        }
+    }
+}
---
+++ Employees/Static.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+     public class Static
+    {
+        private int id;
+        private string name;
+        private double basicsalary;
+        private static int count;
+        public Static(string name,double basicsalary)
+        {
+            count++;
+            this.id = count;
+            this.name = name;
+            this.basicsalary = basicsalary;
+        }
+        public static string TotalCount()
+        {
+            return $"Total count is{count}";
+        }
+        public override string ToString()
+        {
+            return $"Id:{id}Name:{name}Salary:{basicsalary}";
+        }
+     }
+}
